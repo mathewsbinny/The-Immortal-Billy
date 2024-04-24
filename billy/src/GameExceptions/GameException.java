1ef48bda241143691839932b3dc31987c6ff6a67
Alex Pearce <apearce@ggc.edu>
2024-04-23 22:12:16 -0400

    Adding Code for Game Exception

--- billy/src/GameExceptions/GameException.java
+++ billy/src/GameExceptions/GameException.java
@@ -1,4 +1,12 @@
 package GameExceptions;
 
-public class GameException {
-}
+import java.io.IOException;
+
+public class GameException extends IOException {
+    public GameException(){}
+
+    public GameException(String message){
+        super(message);
+    }
+
+} //End InvalidInoutException class
